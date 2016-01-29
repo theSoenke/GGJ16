06b5c425d240a64ef96815f3f3c6e3f41ac9b65b
theSoenke <theSoenke@users.noreply.github.com>
2016-01-29 22:41:33 +0100

    Basic Player Controller

---
+++ Assets/Scripts/Player/MouseRotator.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class MouseRotator : MonoBehaviour
+{
+    public Vector2 rotationRange = new Vector3(70, 70);
+    public float rotationSpeed = 10;
+    public float dampingTime = 0.2f;
+
+    private Vector3 targetAngles;
+    private Vector3 followAngles;
+    private Vector3 followVelocity;
+    private Quaternion originalRotation;
+
+    void Start()
+    {
+        originalRotation = transform.localRotation;
+    }
+    void Update()
+    {
+        transform.localRotation = originalRotation;
+
+        float inputHorizontal = 0;
+        float inputVertical = 0;
+
+        inputHorizontal = Input.GetAxis("Mouse X");
+        inputVertical = Input.GetAxis("Mouse Y");
+
+        if (targetAngles.y > 180)
+        {
+            targetAngles.y -= 360; followAngles.y -= 360;
+        }
+        if (targetAngles.x > 180)
+        {
+            targetAngles.x -= 360; followAngles.x -= 360;
+        }
+        if (targetAngles.y < -180)
+        {
+            targetAngles.y += 360; followAngles.y += 360;
+        }
+        if (targetAngles.x < -180)
+        {
+            targetAngles.x += 360; followAngles.x += 360;
+        }
+
+        targetAngles.y += inputHorizontal * rotationSpeed;
+        targetAngles.x += inputVertical * rotationSpeed;
+
+        targetAngles.y = Mathf.Clamp(targetAngles.y, -rotationRange.y * 0.5f, rotationRange.y * 0.5f);
+        targetAngles.x = Mathf.Clamp(targetAngles.x, -rotationRange.x * 0.5f, rotationRange.x * 0.5f);
+
+
+        followAngles = Vector3.SmoothDamp(followAngles, targetAngles, ref followVelocity, dampingTime);
+        transform.localRotation = originalRotation * Quaternion.Euler(-followAngles.x, followAngles.y, 0);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+
+    public float speed = 8f;                                      
+    public float strafeSpeed = 4f;                                  
+    private float jumpPower = 5f;                                      
+    public bool walkByDefault = true;									
+    public float walkSpeed = 3f;                                     
+    public bool lockCursor = true;
+
+
+    private CapsuleCollider capsule;                                                    
+    private const float jumpRayLength = 0.7f;                                          
+    public bool grounded { get; private set; }
+    private Vector2 input;
+    private IComparer rayHitComparer;
+
+    void Awake()
+    {
+        Instance = this;
+        capsule = GetComponent<Collider>() as CapsuleCollider;
+        grounded = true;
+        Screen.lockCursor = lockCursor;
+        rayHitComparer = new RayHitComparer();
+    }
+
+    void OnDisable()
+    {
+        Screen.lockCursor = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Screen.lockCursor = lockCursor;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool jump = Input.GetButton("Jump");
+
+        bool walkOrRun = Input.GetKey(KeyCode.LeftShift);
+        speed = walkByDefault ? (walkOrRun ? speed : walkSpeed) : (walkOrRun ? walkSpeed : speed);
+
+        input = new Vector2(horizontal, vertical);
+
+        // normalize input if it exceeds 1 in combined length:
+        if (input.sqrMagnitude > 1) input.Normalize();
+
+        // Get a vector which is desired move as a world-relative direction, including speeds
+        Vector3 movement = transform.forward * input.y * speed + transform.right * input.x * strafeSpeed;
+
+        // preserving current y velocity (for falling, gravity)
+        float yv = GetComponent<Rigidbody>().velocity.y;
+
+        // add jump power
+        if (grounded && jump)
+        {
+            yv += jumpPower;
+            grounded = false;
+        }
+
+        // Set the rigidbody's velocity according to the ground angle and desired move
+        GetComponent<Rigidbody>().velocity = movement + Vector3.up * yv;
+
+        // Ground Check:
+        // Create a ray that points down from the centre of the character.
+        Ray ray = new Ray(transform.position, -transform.up);
+
+        // Raycast slightly further than the capsule (as determined by jumpRayLength)
+        RaycastHit[] hits = Physics.RaycastAll(ray, capsule.height * jumpRayLength);
+        System.Array.Sort(hits, rayHitComparer);
+
+
+        if (grounded || GetComponent<Rigidbody>().velocity.y < jumpPower * .5f)
+        {
+            // Default value if nothing is detected:
+            grounded = false;
+            // Check every collider hit by the ray
+            for (int i = 0; i < hits.Length; i++)
+            {
+                // Check it's not a trigger
+                if (!hits[i].collider.isTrigger)
+                {
+                    // The character is grounded, and we store the ground angle (calculated from the normal)
+                    grounded = true;
+
+                    GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0, GetComponent<Rigidbody>().velocity.z);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    //used for comparing distances
+    class RayHitComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((RaycastHit)x).distance.CompareTo(((RaycastHit)y).distance);
+        }
+    }
+}
