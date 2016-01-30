55a5ca7e01d9bc372e2bdc44fcd6362eb8fc7766
theSoenke <theSoenke@users.noreply.github.com>
2016-01-30 01:43:09 +0100

    Simple light trail

---
+++ Assets/Scripts/Player/ShootLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootLight : MonoBehaviour
+{
+    public GameObject lightTrail;
+    public Transform weaponPosition;
+    public float lightSpeed = 100f;
+    public float destructionTime = 5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ThrowLight();
+        }
+    }
+
+    private void ThrowLight()
+    {
+        GameObject trailGo = (GameObject)Instantiate(lightTrail, weaponPosition.position, Quaternion.identity);
+        Rigidbody trailRigidbody = trailGo.GetComponent<Rigidbody>();
+
+        Vector3 forceVector = weaponPosition.TransformDirection(weaponPosition.forward * lightSpeed);
+        trailRigidbody.AddForce(forceVector);
+    }
+}
